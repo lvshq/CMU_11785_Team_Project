f0c8648fde254d4b8bb8301a6c8bc34ebde2b0e3
Fangjing Wu <fangjing.wu@sv.cmu.edu>
2018-11-10 12:46:43 -0800

    training pipeline

---
+++ train.py
@@ -0,0 +1,75 @@
+import numpy as np
+from utils import *
+from Model.train_model import Model
+import argparse
+
+parser = argparse.ArgumentParser(description="arguments")
+parser.add_argument("--epochs", default=50, type=int,
+                    help="The number of epochs to run.")
+parser.add_argument("--lr", default=0.001, type=float,
+                    help="The learning rate.")
+args = parser.parse_args()
+
+
+def compute_acc(model, data, total_labels):
+    tic = time.time()
+    corrects = 0
+    bs = 32
+    for i in range(int(math.ceil(data.shape[0]/bs))):
+        start_idx = (i * bs) % data.shape[0]
+        inputs = data[start_idx:start_idx+bs]
+        labels = total_labels[start_idx:start_idx+bs]
+        inputs = torch.from_numpy(inputs).float()
+        labels = torch.from_numpy(labels).long()
+        outputs = model(inputs)
+        corrects += (outputs.cpu().detach().argmax(dim=1).numpy()==labels.cpu().numpy()).sum() 
+    total_accuracy = corrects / len(total_labels)
+    toc = time.time()
+    print("Time consumed for computing total acc: ", (toc - tic) / 60, " mins.")
+    return total_accuracy
+
+def train_epoch(e, model, optimizer, criterion, train_data, train_labels, test_data, test_labels):
+    batch_size = 32
+    indices = np.random.permutation(train_data.shape[0])
+    for iteration in range(int(math.ceil(train_data.shape[0] / batch_size))):
+        start_idx = (iteration * batch_size) % train_data.shape[0]
+        idx = indices[start_idx:start_idx+batch_size]
+        inputs = train_data[idx]
+        labels = train_labels[idx]
+        inputs = torch.from_numpy(inputs).float()
+        labels = torch.from_numpy(labels).long()
+
+        outputs = model(inputs)
+        train_loss = criterion(outputs, labels)
+
+        train_loss.backward()
+
+        with torch.no_grad():
+            optimizer.step()
+            optimizer.zero_grad()
+
+        running_loss += train_loss.item()
+        interval = 10
+        if iteration % interval == 9:
+            train_accuracy = (outputs.numpy()==labels.cpu().numpy()).mean() 
+            print("Epoch: {} Iteration: {} Training loss: {}, Training accuracy: {}".format((e), (iteration), running_loss/interval, train_accuracy))
+            running_loss = 0.0
+
+    train_accuracy = compute_acc(model, train_data, train_labels)
+    test_accuracy = compute_acc(model, test_data, test_labels)
+    print("Epoch: {} Total Training accuracy: {} Test accuracy: {}".format(e, train_accuracy, test_accuracy))
+
+
+def main():
+    train_data, train_labels = load_data("../existing material/Data/train_imgs/")
+    test_data, test_labels = load_data("../existing material/Data/test_imgs/")
+
+    model = Model()
+    optimizer = torch.optim.Adam(model.parameters(),lr=args.lr, weight_decay=1e-6)
+    criterion = nn.CrossEntropyLoss()
+    for e in args.epochs:
+        train_epoch(e, model, optimizer, criterion, train_data, train_labels, test_data, test_labels)
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,74 @@
+import os
+import numpy as np
+from PIL import Image
+
+def crop_and_scale_image(im):
+    """ Crops and scales a given image. 
+        Args: 
+            im (PIL Image) : image object to be cropped and scaled
+        Returns: 
+            (PIL Image) : cropped and scaled image object
+    """
+    width,height = im.size
+    if width > height:
+        diff = width - height
+        box = diff/2, 0, width - (diff - diff/2), height
+    else:
+        diff = height - width
+        box = 0, diff/2, width, height - (diff - diff/2)
+    im = im.crop(box)
+    toSize = 100,100
+    im= im.resize(toSize, Image.ANTIALIAS)
+    return im
+
+def fname_to_network_input(fname):
+    """ Creates the input for a VGG network from the filename 
+        Args: 
+            fname (string) : the filename to be parsed
+        Returns: 
+            (numpy ndarray) : the array to be passed into the VGG network as a single example
+    """
+    im = Image.open(fname)
+    im = crop_and_scale_image(im)
+    if im.mode is not 'RGB':
+        im = im.convert('RGB')
+    npim = np.asarray(im)
+    npim = np.rollaxis(npim, 2)
+    return npim
+
+label_dict = {}
+idx = 0
+for fname in os.listdir("../existing material/Data/train_imgs/"):
+    label = fname.split('_')[0]
+    if label not in label_dict:
+        label_dict[label] = idx
+        idx += 1
+
+def load_data(path):
+    """  Load training data and test data
+         Args:
+            path (string) : the data path
+         Returns:
+            data, labels (numpy array)
+    """
+    dname = path
+    im_paths = []
+    labels = []
+    for fname in os.listdir(dname):
+        if fname.endswith('.jpg'):
+            label = fname.split('_')[0]
+            if label not in label_dict:
+                print('wrong label ', label)
+            im_paths.append(dname+fname)
+            try:
+                labels.append(label_dict[label])
+            except:
+                continue 
+    data = []
+    for file in im_paths:
+        data.append(fname_to_network_input(file))
+        
+    data = np.asarray(data)
+    labels = np.asarray(labels)
+    return data, labels
+
