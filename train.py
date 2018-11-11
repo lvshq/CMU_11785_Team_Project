31b4d461785a7684525d99f6600320aabf38485c
Ubuntu <lvshq@foxmail.com>
2018-11-11 07:30:58 +0000

    MOD: modify train and util script

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import math
 import time
 import torch
 import torch.nn as nn
+import pdb
 
 parser = argparse.ArgumentParser(description="arguments")
 parser.add_argument("--epochs", default=50, type=int,
@@ -13,40 +14,53 @@ parser.add_argument("--epochs", default=50, type=int,
 parser.add_argument("--lr", default=0.001, type=float,
                     help="The learning rate.")
 args = parser.parse_args()
+TRAIN_IMGS_PATH = "./Data/train_imgs_v0/"
+TEST_IMGS_PATH = "./Data/test_imgs_v0/"
+GPU = torch.cuda.is_available()
 
 
 def compute_acc(model, data, total_labels):
+    """
+    Compute while train and test data accuracy.
+    """
     tic = time.time()
     corrects = 0
     bs = 32
-    for i in range(int(math.ceil(data.shape[0]/bs))):
+    for i in range(int(math.ceil(data.shape[0] / bs))):
         start_idx = (i * bs) % data.shape[0]
-        inputs = data[start_idx:start_idx+bs]
-        labels = total_labels[start_idx:start_idx+bs]
+        inputs = data[start_idx : start_idx+bs]
+        labels = total_labels[start_idx : start_idx + bs]
         inputs = torch.from_numpy(inputs).float()
         labels = torch.from_numpy(labels).long()
+        if GPU:
+            inputs = inputs.cuda()
+            labels = labels.cuda()
         outputs = model(inputs)
-        corrects += (outputs.cpu().detach().argmax(dim=1).numpy()==labels.cpu().numpy()).sum() 
+        corrects += (outputs.cpu().detach().argmax(dim=1).numpy() == labels.cpu().numpy()).sum() 
     total_accuracy = corrects / len(total_labels)
     toc = time.time()
     print("Time consumed for computing total acc: ", (toc - tic) / 60, " mins.")
     return total_accuracy
 
+
 def train_epoch(e, model, optimizer, criterion, train_data, train_labels, test_data, test_labels):
     batch_size = 32
     running_loss = 0.0
     indices = np.random.permutation(train_data.shape[0])
+    if GPU:
+        model = model.cuda()
     for iteration in range(int(math.ceil(train_data.shape[0] / batch_size))):
         start_idx = (iteration * batch_size) % train_data.shape[0]
-        idx = indices[start_idx:start_idx+batch_size]
+        idx = indices[start_idx : start_idx+batch_size]
         inputs = train_data[idx]
         labels = train_labels[idx]
         inputs = torch.from_numpy(inputs).float()
         labels = torch.from_numpy(labels).long()
-
+        if GPU:
+            inputs = inputs.cuda()
+            labels = labels.cuda()
         outputs = model(inputs)
         train_loss = criterion(outputs, labels)
-
         train_loss.backward()
 
         with torch.no_grad():
@@ -57,6 +71,7 @@ def train_epoch(e, model, optimizer, criterion, train_data, train_labels, test_d
         interval = 10
         if iteration % interval == 9:
             outputs = outputs.cpu().detach().argmax(dim=1)
+            #pdb.set_trace()
             train_accuracy = (outputs.numpy()==labels.cpu().numpy()).mean() 
             print("Epoch: {} Iteration: {} Training loss: {} Training accuracy: {}".format((e), (iteration), running_loss/interval, train_accuracy))
             running_loss = 0.0
@@ -74,8 +89,9 @@ def train_epoch(e, model, optimizer, criterion, train_data, train_labels, test_d
 
 
 def main():
-    train_data, train_labels = load_data("../existing material/Data/train_imgs/")
-    test_data, test_labels = load_data("../existing material/Data/test_imgs/")
+    train_data, train_labels = load_data(TRAIN_IMGS_PATH)
+    test_data, test_labels = load_data(TEST_IMGS_PATH)
+    train_data = preprocess(train_data)
     print(train_data.shape, train_labels.shape)
     print(test_data.shape, test_labels.shape)
 
--- utils.py
+++ utils.py
@@ -2,6 +2,10 @@ import os
 import numpy as np
 from PIL import Image
 
+IMG_SIZE= (150, 150)
+TRAIN_IMGS_PATH = "./Data/train_imgs_v0/"
+TEST_IMGS_PATH = "./Data/test_imgs_v0"
+
 def crop_and_scale_image(im):
     """ Crops and scales a given image. 
         Args: 
@@ -17,9 +21,32 @@ def crop_and_scale_image(im):
         diff = height - width
         box = 0, diff/2, width, height - (diff - diff/2)
     im = im.crop(box)
-    toSize = 150,150
-    im= im.resize(toSize, Image.ANTIALIAS)
+    im= im.resize(IMG_SIZE, Image.ANTIALIAS)
     return im
+
+
+def sample_zero_mean(x):
+    return x - np.mean(x, axis=1, keepdims=True)
+
+
+def gcn(x, scale=55., bias=0.01):
+    return scale * x / np.sqrt(bias + np.var(x, axis=1, keepdims=True))
+
+
+def preprocess(x):
+    """
+    Flatten train data and make each sample have a mean of zero.
+    Also GCN each sample (assume sample mean=0).
+    Finally restore original shape.
+    """
+    sample_num = x.shape[0]
+    original_shape = x.shape
+    x = np.reshape(x, (sample_num, -1))
+    x = sample_zero_mean(x)
+    x = gcn(x)
+    x = np.reshape(x, original_shape)
+    return x
+
 
 def fname_to_network_input(fname):
     """ Creates the input for a VGG network from the filename 
@@ -36,13 +63,15 @@ def fname_to_network_input(fname):
     npim = np.rollaxis(npim, 2)
     return npim
 
-label_dict = {}
-idx = 0
-for fname in os.listdir("../existing material/Data/train_imgs/"):
-    label = fname.split('_')[0]
-    if label not in label_dict:
-        label_dict[label] = idx
-        idx += 1
+
+def get_label(fname):
+    """
+    The image filename is in format: label_xxx.jpg, 
+    where xxx is the number of this image in such label.
+    """
+    label = "".join(fname.split('_')[:-1])
+    return label
+
 
 def load_data(path):
     """  Load training data and test data
@@ -56,7 +85,7 @@ def load_data(path):
     labels = []
     for fname in os.listdir(dname):
         if fname.endswith('.jpg'):
-            label = fname.split('_')[0]
+            label = get_label(fname)
             if label not in label_dict:
                 print('wrong label ', label)
             im_paths.append(dname+fname)
@@ -71,4 +100,51 @@ def load_data(path):
     data = np.asarray(data)
     labels = np.asarray(labels)
     return data, labels
+
+
+def load_data(path):
+    """  Load training data and test data
+         Args:
+            path (string) : the data path
+         Returns:
+            data, labels (numpy array)
+    """
+    dname = path
+    im_paths = []
+    labels = []
+    for fname in os.listdir(dname):
+        if fname.endswith('.jpg'):
+            label = get_label(fname)
+            if label not in label_dict:
+                print('wrong label ', label)
+            im_paths.append(dname+fname)
+            try:
+                labels.append(label_dict[label])
+            except:
+                continue 
+    data = []
+    for file in im_paths:
+        data.append(fname_to_network_input(file))
+        
+    data = np.asarray(data)
+    labels = np.asarray(labels)
+    return data, labels
+
+
+label_dict = {}
+idx = 0
+for fname in os.listdir(TRAIN_IMGS_PATH):
+    label = get_label(fname)
+    if label not in label_dict:
+        label_dict[label] = idx
+        idx += 1
+# Ensure test data's label is same with train data
+for fname in os.listdir(TEST_IMGS_PATH):
+    label = get_label(fname)
+    if label not in label_dict:
+        raise Exception("Invalid label: %s" % label)
+
+#test
+for label in label_dict:
+    print(label)
 
