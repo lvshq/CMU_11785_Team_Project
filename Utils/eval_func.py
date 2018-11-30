a3aa1afe4cdc74763f3831767e922d40df53811e
Hao Tang <haot1@andrew.cmu.edu>
2018-11-30 12:01:20 -0800

    dim fixed

--- Utils/eval_func.py
+++ Utils/eval_func.py
@@ -1,9 +1,9 @@
 import torch
 import heapq
-
+import numpy as np
 def eval(embeddings, labels, num_sample):
     """
-    :param embeddings: list of tensors,all the embeddings
+    :param embeddings: list of tensors,each tensor should be a horizontal vector
     :param labels: all the labels associate with the embeddings
     :param num_sample: how many neighbours are we drawing
     :return:
@@ -14,7 +14,7 @@ def eval(embeddings, labels, num_sample):
         for i2,emb2 in enumerate(embeddings):
             if i == i2:
                 continue
-            sim=torch.nn.functional.cosine_similarity(emb,emb2)
+            sim=torch.nn.functional.cosine_similarity(emb,emb2,dim=0)
             heapq.heappush(temp,[sim,labels[i2]])
             if len(temp)>num_sample:
                 heapq.heappop(temp)
